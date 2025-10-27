28bea368e5318d5259632ecfec557d61b637f2d9
TruongVu2511 <truongvun781@gmail.com>
2025-10-27 21:11:55 +0700

    Update Bài 6_Session4.c

--- Bài 6_Session4.c
+++ Bài 6_Session4.c
@@ -2,8 +2,7 @@
 
 int main() {
     int chi_so_cu, chi_so_moi;
-    int so_dien;
-    int tien_dien;
+    int so_dien, tien_dien;
 
     printf("Nhap chi so cu: ");
     scanf("%d", &chi_so_cu);
@@ -11,16 +10,32 @@ int main() {
     scanf("%d", &chi_so_moi);
 
     so_dien = chi_so_moi - chi_so_cu;
-
-    if (so_dien < 0) {
-        printf("Chi so moi phai lon hon chi so cu!\n");
-        return 0;
-    }
+    printf("So dien tieu thu: %d kWh\n", so_dien);
 
     if (so_dien < 50) {
         tien_dien = so_dien * 10000;
-    } else if (so_dien < 100) {
+        printf("Tien dien phai tra: %d VND (muc 1 - 10.000 VND/kWh)\n", tien_dien);
+    } 
+    else if (so_dien < 100) {
         tien_dien = so_dien * 15000;
-    } else if (so_dien < 150) {
-        tien_dien =_
+        printf("Tien dien phai tra: %d VND (muc 2 - 15.000 VND/kWh)\n", tien_dien);
+    } 
+    else if (so_dien < 150) {
+        tien_dien = so_dien * 20000;
+        printf("Tien dien phai tra: %d VND (muc 3 - 20.000 VND/kWh)\n", tien_dien);
+    } 
+    else if (so_dien < 200) {
+        tien_dien = so_dien * 25000;
+        printf("Tien dien phai tra: %d VND (muc 4 - 25.000 VND/kWh)\n", tien_dien);
+    } 
+    else {
+        tien_dien = so_dien * 30000;
+        printf("Tien dien phai tra: %d VND (muc 5 - 30.000 VND/kWh)\n", tien_dien);
+    }
 
+    return 0;
+}
+
+
+
+           
